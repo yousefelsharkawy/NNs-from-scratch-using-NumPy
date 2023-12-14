ad4ab886112180505aa7341eb522aa1a90ce322e
yousefelsharkawy <yousefelsharkawy111@gmail.com>
2023-12-14 13:54:54 +0200

    - corrections in the cost function calculations for the minibatches

--- general classifier/classifier_NN.py
+++ general classifier/classifier_NN.py
@@ -6,8 +6,12 @@ import numpy as np
 # create a binary classifier using a neural network classifier
 class ClassifierNN:
 
+
+    ## initialization function of the model 
     def __init__(self, layer_dims,activations,initialization_method = "he"):
+        # layer_dims is a list containing the number of neurons in each layer starting from the input layer
         self.layer_dims = layer_dims
+        # activations is a list containing the activation functions of each layer starting from the first hidden layer, that is why the length of the list is equal to the number of hidden layers
         assert (len(layer_dims) - 1) == len(activations), "The number of hidden layers and activations must be the same"
         self.activations = activations
         self.num_layers = len(layer_dims)
@@ -15,7 +19,7 @@ class ClassifierNN:
         self.costs = []
         
 
-    
+    ## initialize the parameters of the model
     def initialize_parameters(self, initialization_method):
         np.random.seed(3)
         #assert self.layer_dims[-1] == 1, "The output layer must have 1 neuron"
@@ -23,43 +27,43 @@ class ClassifierNN:
         for l in range(1, self.num_layers): # the index of the last layer is num_layers - 1 that's why num_layers is not included
             #print(l)
             if initialization_method == "random":
+                # in random initialization we sample from a normal distribution with mean 0 and variance 1 and then multiply by 0.01 to make the variance 0.01 -small weights-
                 parameters['W' + str(l)] = np.random.randn(self.layer_dims[l], self.layer_dims[l-1]) * 0.01
                 parameters['b' + str(l)] = np.zeros((self.layer_dims[l], 1))
             elif initialization_method == "he":
+                # in he initialization we sample from a normal distribution with mean 0 and variance 2/n[l-1] where n[l-1] is the input to the layer 
                 parameters['W' + str(l)] = np.random.randn(self.layer_dims[l], self.layer_dims[l-1]) * np.sqrt(2/self.layer_dims[l-1])
                 parameters['b' + str(l)] = np.zeros((self.layer_dims[l], 1))
         return parameters
     
-    # define the activation functions
-    def sigmoid(self, Z):
-        return 1 / (1 + np.exp(-Z))
-
-    def relu(self, Z):
-        return np.maximum(0, Z)
     
-    def tanh(self, Z):
-        return np.tanh(Z)
-    
-    def softmax(self, Z):
-        return np.exp(Z) / np.sum(np.exp(Z), axis=0, keepdims=True)
-    
-    # define the forward propagation
+    ## define the forward propagation
     def forward_propagation(self, X, keep_prob = None, parameters = None):
+        # we do this to allow the user to pass parameters to the forward propagation function anywhere outside, if he didn't pass any parameters, we will use the parameters of the model
         if parameters is None:
             parameters = self.parameters
         cashes = {'A0': X} # this is not efficient with larger datasets
         A = X # first activation is the input layer 
+        
         # dropout on the input layer
         if keep_prob is not None:
+            # D will be of the same size as A, and will contain numbers between 0 and 1
             D = np.random.rand(A.shape[0], A.shape[1])
-            D = (D < keep_prob[0]).astype(int) # convert the matrix to 0s and 1s
+            D = (D < keep_prob[0]).astype(int) # convert the matrix to 0s and 1s, with 0s having a probability of keep_prob[0] and 1s having a probability of 1 - keep_prob[0]
+            # shut down some neurons of A 
             A = np.multiply(A, D)
+            # divide A by keep_prob[0] to keep the expected value of A the same as before dropping out some of the neurons
             A /= keep_prob[0]
+            # cache the D matrix to use it in the backward propagation by multiplying them with the upstream derivatives dA
             cashes['D0'] = D
 
         
         for l in range(1, self.num_layers):
+            # Z calculation for the current layer Z = W.A + b
             Z = np.dot(parameters['W' + str(l)], A) + parameters['b' + str(l)]
+
+            ## apply the activation function on Z to get A
+            # we check the activation function of the current layer - we use l-1 because we start activations with index 0 and layers with index 1 - as we have an index mismatch between the two lists
             if self.activations[l-1] == 'sigmoid':
                 A = self.sigmoid(Z)
             elif self.activations[l-1] == 'relu':
@@ -68,6 +72,8 @@ class ClassifierNN:
                 A = self.tanh(Z)
             elif self.activations[l-1] == 'softmax':
                 A = self.softmax(Z)
+
+
             ## apply dropout
             if keep_prob != None:
                 print("from inside dropout")
@@ -80,61 +86,71 @@ class ClassifierNN:
                     A /= keep_prob[l] # divide by the keep probability to keep the expected value of the activations the same as before dropping out some of them
                     cashes['D' + str(l)] = D # save the D matrix to use it in the backward propagation
                     # print("D shape = ", cashes['D' + str(l)].shape)
+            
+            ## cache the Z and A of the current layer to use them in the backward propagation
             cashes['Z' + str(l)] = Z
             cashes['A' + str(l)] = A
         return A, cashes
     
+    
+    ## forward propagation helper functions
+
+    # define the activation functions
+    def sigmoid(self, Z):
+        return 1 / (1 + np.exp(-Z)) # 1 / (1 + e^-Z)
+
+    def relu(self, Z):
+        return np.maximum(0, Z) # max(0, Z)
+    
+    def tanh(self, Z):
+        return np.tanh(Z) # tanh(Z)
+    
+    def softmax(self, Z):
+        # we sum over the rows - we sum the different classes outputs for the same example then we apply e^z and divide each element by the sum, we do that for each and every example
+        return np.exp(Z) / np.sum(np.exp(Z), axis=0, keepdims=True)
+    
+    
     # define the cost function
-    def compute_cost(self, A, Y, M, lambd = 0, loss = 'BinaryCrossEntropy'):
-        # handle the case when A is 0 or 1
+    def compute_cost(self, A, Y, lambd = 0, loss = 'BinaryCrossEntropy'):
+        m = Y.shape[1]
+        # handle the case when A is 0 or 1, as log(0) is undefined
         A[A == 0] = 1e-10
         A[A == 1] = 1 - 1e-10
         if loss == 'BinaryCrossEntropy':
-            cost = (-1/ M) * (np.sum(Y*np.log(A) + (1-Y)*np.log(1-A)))
+            cost = (-1/ m) * (np.sum(Y*np.log(A) + (1-Y)*np.log(1-A)))
         elif loss == 'CategoricalCrossEntropy':
-            cost = (-1/ M) * (np.sum(Y*np.log(A)))
+            cost = (-1/ m) * (np.sum(Y*np.log(A)))
         elif loss == "SpareCategoricalCrossEntropy":
-            selected_probs = A[Y, np.arange(M)] # np.arange(M) will create an array of numbers from 0 to M-1 which we will use to index the columns of A increasing by 1 each time
-            cost = (-1/ M) * (np.sum(np.log(selected_probs)))
+            selected_probs = A[Y, np.arange(m)] # np.arange(M) will create an array of numbers from 0 to M-1 which we will use to index the columns of A increasing by 1 each time
+            cost = (-1/ m) * (np.sum(np.log(selected_probs)))
+        
         ## add the L2 regularization cost
         if lambd != 0:
             L2_regularization_cost = 0
             for l in range(1, self.num_layers):
                 L2_regularization_cost += np.sum(np.square(self.parameters['W' + str(l)]))
-            L2_regularization_cost *= (lambd/(2*M))
+            L2_regularization_cost *= (lambd/(2*m))
             cost += L2_regularization_cost
         return cost
     
-    # define the backward activations they take dA and return dZ by multiplying dA by the derivative of the activation function element wise
-    def sigmoid_backward(self, dA, A):
-        g_dash = A * (1 - A)
-        return dA * g_dash
     
-    def relu_backward(self, dA, Z):
-        # we wil multiply dA by 1 if Z > 0 and 0 if Z <= 0, so instead we pass dA if Z > 0 and 0 if Z <= 0
-        dZ = np.array(dA, copy=True)
-        dZ[Z <= 0] = 0 # keep dA and reset the rest -where dZ <= 0-  to 0
-        return dZ
     
-    def tanh_backward(self, dA, A):
-        g_dash = (1 - np.power(A, 2))
-        return dA * g_dash
     
-    def softmax_backward(self, dA, A):
-        g_dash = A * (1 - A)
-        return dA * g_dash
-    
-    # define the backward propagation
+    ##  backward propagation
     def backward_propagation(self, A, Y, cashes, lambd = 0, keep_prob = None, loss = 'BinaryCrossEntropy'):
         grads = {}
         m = Y.shape[1]
+        
+        ## calculation of the first upstream derivative dA - which is the derivative of the cost function with respect to Y_hat -
         if loss == 'BinaryCrossEntropy':
-            dA = - (np.divide(Y, A) - np.divide(1 - Y, 1 - A)) # this is the derivative of the cost function with respect to A
-        elif loss == 'CategoricalCrossEntropy':
-            dA = - (np.divide(Y, A))
+            dA = - (1 / m) * (np.divide(Y, A) - np.divide(1 - Y, 1 - A)) # this is the derivative of the cost function with respect to A
+        elif loss == 'CategoricalCrossEntropy': 
+            # Y is of shape (C, M) , written as 1-hot matrix, A is of shape (C, M) 
+            # the cost function is -1/M * sum(Y * log(A)) , so the derivative of the cost function with respect to A is -1/M * Y/A
+            dA = - (1 / m) * (np.divide(Y, A)) 
         elif loss == "SpareCategoricalCrossEntropy":
             Y = np.eye(A.shape[0])[Y.reshape(-1)].T # Y is a vector of shape (1, M) and we want to convert it to a matrix of shape (C, M) where C is the number of classes
-            dA = - (np.divide(Y, A))
+            dA = - (1 / m) * (np.divide(Y, A))
 
         # apply the backward activations
         for l in reversed(range(1, self.num_layers)):
@@ -166,6 +182,29 @@ class ClassifierNN:
             grads['db' + str(l)] = db
             grads['dW' + str(l)] = dW
         return grads
+    
+    
+    ## backward propagation helper functions
+
+    # define the backward activations they take dA and return dZ by multiplying dA by the derivative of the activation function element wise
+    def sigmoid_backward(self, dA, A):
+        g_dash = A * (1 - A)
+        return dA * g_dash
+    
+    def relu_backward(self, dA, Z):
+        # we wil multiply dA by 1 if Z > 0 and 0 if Z <= 0, so instead we pass dA if Z > 0 and 0 if Z <= 0
+        dZ = np.array(dA, copy=True)
+        dZ[Z <= 0] = 0 # keep dA and reset the rest -where dZ <= 0-  to 0
+        return dZ
+    
+    def tanh_backward(self, dA, A):
+        g_dash = (1 - np.power(A, 2))
+        return dA * g_dash
+    
+    def softmax_backward(self, dA, A):
+        g_dash = A * (1 - A)
+        return dA * g_dash
+    
     
     # define the update parameters
     def update_parameters(self, grads, learning_rate, adam_counter, optimizer = "gd", beta1 = 0.9, beta2 = 0.999, epsilon = 1e-8):
@@ -208,7 +247,7 @@ class ClassifierNN:
     def train(self, X, Y , learning_rate, num_epochs, batch_size = 64, lambd = 0, keep_prob = None, print_cost = True, loss = 'BinaryCrossEntropy', optimizer = "gd", beta1 = 0.9, beta2 = 0.999, epsilon = 1e-8):
         # preparing some important variables
         seed = 10 # The seed for randomly generating the mini batches each epoch
-        M = X.shape[1] # number of training examples in the whole dataset, we use it to divide the accumulated cost by it to get the average cost of all examples
+        #M = X.shape[1] # number of training examples in the whole dataset, we use it to divide the accumulated cost by it to get the average cost of all examples
         adam_counter = 0 # this variable is used to count the number of iterations of the adam optimizer, it is used to correct the values of v and s in the adam optimizer
         if keep_prob != None:
             assert len(keep_prob) == self.num_layers - 1, "The number of keep probabilities must be the same as the number of hidden layers + the input layer"
@@ -233,7 +272,7 @@ class ClassifierNN:
             for mini_batch in mini_batches:
                 X_batch, Y_batch = mini_batch
                 Y_prid_batch, cashes = self.forward_propagation(X_batch,keep_prob)
-                cost = self.compute_cost(Y_prid_batch, Y_batch, M, lambd, loss)
+                cost = self.compute_cost(Y_prid_batch, Y_batch, lambd, loss)
                 total_cost += cost
                 #self.costs.append(cost)
                 grads = self.backward_propagation(Y_prid_batch, Y_batch, cashes, lambd, keep_prob, loss)
